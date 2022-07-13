e26ad3ad0c56dbaed44d2db61d63a6c7659e92f5
mmhunter3515 <jn_kissmk@126.com>
2022-07-13 21:47:46 +0800

    dfsdf

--- wp-content/themes/dux/action/load.php
+++
@@ -1,4 +0,0 @@
-<br>
-<b>Deprecated</b>:  Array and string offset access syntax with curly braces is deprecated in <b>/www/wwwroot/smsmoney/wp-content/themes/dux/action/load.php</b> on line <b>45</b><br>
-<br>
-<b>Deprecated</b>:  Array and string offset access syntax with curly braces is deprecated in <b>/www/wwwroot/smsmoney/wp-content/themes/dux/action/load.php</b> on line <b>63</b><br>
