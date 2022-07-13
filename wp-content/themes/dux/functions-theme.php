78d5fb50ada9e985af05ad41d3a84a758c7df107
mmhunter3515 <jn_kissmk@126.com>
2022-07-13 18:25:08 +0800

    commit!

---
+++ wp-content/themes/dux/functions-theme.php
@@ -0,0 +1,9 @@
+<br>
+<b>Deprecated</b>:  Array and string offset access syntax with curly braces is deprecated in <b>/www/wwwroot/smsmoney/wp-content/themes/dux/functions-theme.php</b> on line <b>617</b><br>
+<br>
+<b>Deprecated</b>:  Array and string offset access syntax with curly braces is deprecated in <b>/www/wwwroot/smsmoney/wp-content/themes/dux/functions-theme.php</b> on line <b>635</b><br>
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function get_stylesheet_directory() in /www/wwwroot/smsmoney/wp-content/themes/dux/functions-theme.php:12
+Stack trace:
+#0 {main}
+  thrown in <b>/www/wwwroot/smsmoney/wp-content/themes/dux/functions-theme.php</b> on line <b>12</b><br>
