1db37485e2978a17138f401bfb7e5c8531ce5287
mmhunter3515 <jn_kissmk@126.com>
2023-06-30 03:54:26 +0800

    sdfsdf

--- wp-content/themes/twentytwentytwo/inc/block-patterns.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in /www/wwwroot/githubtelegram/wp-content/themes/twentytwentytwo/inc/block-patterns.php:135
-Stack trace:
-#0 {main}
-  thrown in <b>/www/wwwroot/githubtelegram/wp-content/themes/twentytwentytwo/inc/block-patterns.php</b> on line <b>135</b><br>
